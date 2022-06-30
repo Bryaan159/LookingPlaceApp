e81bf06713c7d1b5b862a506bfb12e2efc5ddd55
Sirenoman <00003120@uca.edu.sv>
2022-06-29 19:19:51 -0600

    Cambios en tabla categoria y agregando fragmento de listasSitiosTuristicos

--- app/src/main/java/com/hernandez/lookingplace/tablaCategoria.kt
+++ app/src/main/java/com/hernandez/lookingplace/tablaCategoria.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.hernandez.lookingplace.databinding.FragmentTablaCategoriaBinding
 
 class tablaCategoria : Fragment() {
+
+    private lateinit var binding: FragmentTablaCategoriaBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tabla_categoria, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tabla_categoria, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        val navController = findNavController()
+        binding.cartaCiclismo.setOnClickListener {
+            val funcion = tablaCategoriaDirections.actionTablaCategoriaToListaSitiosTuristicos()
+            navController.navigate(funcion)
+        }
     }
 
 }
